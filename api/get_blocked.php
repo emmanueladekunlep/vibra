29bd45c00dadc64752ecc49901641b38f58e1909
Emmanuel Adekunle Peace <07032977572@users.noreply.github.com>
2026-09-07 02:25:40 +0100

    update project

--- api/get_blocked.php
+++
@@ -1,8 +0,0 @@
-<?php
-// VIBRA - Get Blocked Users API
-// File: /api/get_blocked.php
-
-require_once 'config.php';
-
-// Return empty array since blocking is handled client-side
-sendSuccess(['blocked' => []]);
